00e6d1659ec1045ab8fe54073a76f1ade4f00ece
ThejasNK <63611800+ThejasNK@users.noreply.github.com>
2020-07-16 13:54:59 +0530

    assignment 2 (15/7/2020)
    question 3(optional question)

---
+++ day2 assig/ques3.js
@@ -0,0 +1,8 @@
+alert('This program gets ur age and tells whether ur eligible for drinking');
+var age=prompt("Enter your age ");
+if age>21{
+    console.log("eligible to drink");
+}
+else {
+    console.log("not eligible to drink");
+}
